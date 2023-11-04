881c37466bcfb53f2648e62e4ce7045359925572
Logan Tyran <logantyran6@gmail.com>
2023-11-04 11:34:50 -0400

    Multiplayer menu

---
+++ Scripts/ClientServer.cs
@@ -0,0 +1,67 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marbles.Scripts;
+
+public partial class ClientServer : Node
+{
+    private const int PORT = 8910;
+    private ENetMultiplayerPeer _peer;
+
+    private Resource _resource;
+
+    public override void _Ready()
+    {
+        _peer = new ENetMultiplayerPeer();
+    }
+
+    public void Host()
+    {
+        GD.Print(_peer.CreateServer(PORT));
+        GD.Print($"Server: {Multiplayer.IsServer()}");
+        Multiplayer.PeerConnected += Server_PeerConnected;
+
+        Multiplayer.MultiplayerPeer = _peer;
+    }
+
+    private void Server_PeerConnected(long id)
+    {
+        RpcId(id, "Hello", new Vector3(1, 1, 0), new Vector3(9, 9, 9));
+        GD.Print("Hello", new Vector3(1, 1, 0), new Vector3(9, 9, 9));
+    }
+
+    public struct PlayerPacket
+    {
+        public Vector3 Velocity = new Vector3(1, 0, 1);
+
+        public PlayerPacket()
+        {
+
+        }
+    }
+
+    [Rpc(MultiplayerApi.RpcMode.Authority)]
+    public void Hello(Vector3 position, Vector3 velocity)
+    {
+        GD.Print($"RPC: {position} {velocity}");
+    }
+
+    public void Join(string ip)
+    {
+        Multiplayer.ConnectedToServer += Multiplayer_ConnectedToServer;
+
+        GD.Print(_peer.CreateClient(ip, PORT));
+        GD.Print($"Client: {!Multiplayer.IsServer()}");
+        Multiplayer.MultiplayerPeer = _peer;
+    }
+
+    private void Multiplayer_ConnectedToServer()
+    {
+        GD.Print("Connected");
+    }
+}
--- Scripts/MainMenu.cs
+++ Scripts/MainMenu.cs
@@ -1,4 +1,5 @@
 using Godot;
+using Marbles.Scripts;
 using System;
 
 public partial class MainMenu : Node2D
@@ -6,6 +7,8 @@ public partial class MainMenu : Node2D
     private Control _rootMenu;
 
     private Control _joinSubmenu;
+    private Button _connect;
+    private Button _host;
     private Button _join;
 	private Button _exit;
 
@@ -14,20 +17,41 @@ public partial class MainMenu : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+        _join = (Button)FindChild("Host");
+        _join.Pressed += _host_Pressed;
+
         _join = (Button)FindChild("Join");
         _join.Pressed += _join_Pressed;
+
+        _connect = (Button)FindChild("Connect");
+        _connect.Pressed += _connect_Pressed;
 
         _exit = (Button)FindChild("Exit");
         _exit.Pressed += _exit_Pressed;
 
         _joinSubmenu = (Control)FindChild("JoinSubMenu");
         _joinSubmenu.Visible = false;
+
+
+
         _rootMenu = (Control)FindChild("RootMenu");
         _rootMenu.Visible = true;
 
         _joinSubMenuBack = (Button)_joinSubmenu.FindChild("Back");
         _joinSubMenuBack.Pressed += _joinSubMenuBack_Pressed;
 
+    }
+
+    private void _connect_Pressed()
+    {
+        ((ClientServer)FindParent("Node3D").FindChild("Session").FindChild("ClientServer")).Join("127.0.0.1");
+    }
+
+    private void _host_Pressed()
+    {
+        ((ClientServer)FindParent("Node3D").FindChild("Session").FindChild("ClientServer")).Host();
+
+        Visible = false;
     }
 
     private void _joinSubMenuBack_Pressed()
@@ -52,6 +76,7 @@ public partial class MainMenu : Node2D
         _exit.Pressed -= _exit_Pressed;
         _join.Pressed -= _join_Pressed;
         _joinSubMenuBack.Pressed -= _joinSubMenuBack_Pressed;
+        _connect.Pressed -= _connect_Pressed;
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
